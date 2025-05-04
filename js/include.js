e29f87fd1b367cc40003c99cea2ed60e5422ba55
Марія Дема <mashadema04@gmail.com>
2025-05-05 00:48:58 +0300

    first commit

---
+++ js/include.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./partials/header.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("header-placeholder").innerHTML = data;
+      });
+  
+    fetch("./partials/footer.html")
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById("footer-placeholder").innerHTML = data;
+      });
+  });
+  
